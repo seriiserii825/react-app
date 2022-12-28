b949b00d230be27f22361da1cc4d358a19dff0ce
serii <seriiburduja@gmail.com>
2022-12-28 17:29:17 +0200

    modeal

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,11 +1,16 @@
 import { useEffect } from 'react';
+import { useRef } from 'react';
 export default function Modal({ modal_content, closeModal }) {
+	let timeoutId = useRef(null);
 
 	useEffect(() => {
-		setTimeout(() => {
+		timeoutId.current = setTimeout(() => {
 			closeModal();
 		}, 3000);
-	}, []);
+		return () => {
+			clearTimeout(timeoutId.current);
+		};
+	}, [closeModal]);
 
 	return (
 		<div className="modal">
